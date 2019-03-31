e18dc8a5812704dce3d4d20e06a801620bc8e1f0
VFDan <44413950+VFDan@users.noreply.github.com>
2019-03-31 12:14:17 -0400

    Update Palindromic Plugin.js

--- Plugins/Palindromic Plugin.js
+++ Plugins/Palindromic Plugin.js
@@ -1,6 +1,6 @@
-Number.prototype.reverseAdd = function() {
+Number.prototype.reverse = function() {
   return Number(this.toString().split('').reverse().join(''));
 }
 Number.prototype.isPalindromic = function() {
-  return Number(this) === this.reverseAdd();
+  return Number(this) === this.reverse();
 }
