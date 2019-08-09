4cfdb30ef80a82abd17b4d3ac6b08b6587650577
VFDan <44413950+VFDan@users.noreply.github.com>
2019-08-08 21:57:45 -0400

    Update download.js
    Fixed bug, made export

--- resources/download.js
+++ resources/download.js
@@ -1,6 +1,8 @@
+/* exported download */
+
 function download(link) {
   let anchor = document.createElement('a');
   anchor.target = '_blank';
-  anchor.download = data.link;
+  anchor.download = link;
   anchor.click();
 }
