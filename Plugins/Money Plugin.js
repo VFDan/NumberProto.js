c3303eac2d829f9cef6a551aa6cf4045aa6de77f
VFDan <44413950+VFDan@users.noreply.github.com>
2020-04-22 19:29:38 -0400

    Delete this along with the rest of the Plugins folder

--- Plugins/Money Plugin.js
+++
@@ -1,6 +0,0 @@
-Number.prototype.discount = function(percent) {
-  return (this - this * (percent / 100)).round(2);
-};
-Number.prototype.tax = function(percent) {
-  return (this + this * (percent/100)).round(2);
-}
